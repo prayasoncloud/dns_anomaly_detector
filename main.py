909b40fa3ec6d1b5a87dd58ddf1b0de557921995
Prayas Patel <107023940+prayasoncloud@users.noreply.github.com>
2026-05-06 10:30:48 +0000

    Add sklearn isolationforest model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 import time
 from collections import Counter 
 import math
+from sklearn.ensemble import IsolationForest
 
 
 def calculate_entropy(domains):
@@ -16,8 +17,10 @@ def calculate_entropy(domains):
 
     entropy = 0
     for count in freq.values():
-    p = count / total
-    entropy -= p * math.log2(p)
+        p = count / total
+        entropy -= p * math.log2(p)
+
+    return entropy
 
 
 cmd = [
@@ -47,7 +50,9 @@ domains = []
 window_start = time.time()
 Window_size = 30
 
-
+model = IsolationForest(contamination=0.1)
+feature_history=[]
+model_trained = False
 
 while True:
     try:
@@ -62,7 +67,7 @@ while True:
         for i, part in enumerate(parts):
             if part == "A?" or part == "AAAA?":
                 if i + 1 < len(parts):
-                    domain = parts[i + 1]
+                    domain = parts[i + 1].rstrip(".")
 
                     domains.append(domain)
 
@@ -73,7 +78,23 @@ while True:
             unique_domains = len(set(domains))
             entropy = calculate_entropy(domains)
 
-            print(f"[FEATURES] {total_domains}, {unique_domains}, {round(entropy, 3)}")
+            # print(f"[FEATURES] {total_domains}, {unique_domains}, {round(entropy, 3)}")
+            features = [total_domains, unique_domains, entropy]
+            feature_history.append(features)
+
+            if len(feature_history) >= 20 and model_trained == False:
+                model.fit(feature_history)
+                model_trained = True
+                print("[1]Model have been trained at first\n")
+
+            if model_trained == True:
+                prediction = model.predict([features])[0]
+
+                if prediction == -1:
+                    print (f"anomaly_detected {features}\n")
+
+                else:
+                    print(f"Normal {features}\n")
 
             
             domains = []
